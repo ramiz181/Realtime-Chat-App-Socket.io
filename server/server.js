330aea3cc40758f1df73684ddf711fc893a55a8f
ramiz181 <ramizmalik78685@gmail.com>
2026-02-03 23:45:05 +0500

    chat-app socket.io integrated, handled realtime chat on client and server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+const users = {}
+
+app.use(express.static('./public/client/'))
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+})
+
+io.on('connection', socket => {
+
+    socket.on('new-user-joined', user => {
+        users[socket.id] = user
+        socket.broadcast.emit('user-joined', `${user} just joined`)
+    })
+
+    socket.on('sent-message', message => {
+        console.log("this: ", message);
+        socket.broadcast.emit('client-message', message)
+    })
+})
+
+
+server.listen(8000, () => console.log("server started"))
